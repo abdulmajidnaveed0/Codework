f31542319bb337dcbdd9983ad88aee8b2d8eeb68
abdulmajidnaveed0 <abdulmajidnaveed0@gmail.com>
2023-12-11 21:02:11 -0500

    Code for Q2
    Code for Q2.

---
+++ NET022Exam2Code/Question2/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Dictionary<string, string> d;
+
+        }
+
+
+        public class ArgforEventArgs: EventArgs
+        {
+            public string data { get; set; }  
+        }
+
+        public class Service
+        {
+            public event EventHandler<ArgforEventArgs> Callback;
+
+
+            public void RaisetheEvent()
+            {
+                ArgforEventArgs args = new ArgforEventArgs();
+                RaiseEvent(args);
+            }
+
+            public virtual void RaiseEvent(ArgforEventArgs e)
+            {
+                Callback?.Invoke(this, e);
+            }
+
+        }
+        public class MainForEventArgs
+        {
+            Service service = new Service();
+            //Service.
+        }
+
+
+
+        public class GenericClass<T> where T:struct
+        {
+
+            private List<T> collection;
+
+            public GenericClass()
+            {
+                collection = new List<T>();
+            }
+            public void AddItem(T item)
+            {
+                collection.Add(item);
+
+            }
+            public T GetItem(int index)
+            {
+                try
+                {
+                    return collection[index];
+                }
+                catch
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+
+            public IList<T> GetSorted()
+            {
+                var sortedC = new List<T>();
+                collection.AddRange ( sortedC);
+                sortedC.Sort();
+                return sortedC;
+            }
+        }
+
+
+
+    }
+}
