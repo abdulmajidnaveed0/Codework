36c2aaa7b16ebdafa736e00b4f5445a3de3a2b36
abdulmajidnaveed0 <abdulmajidnaveed0@gmail.com>
2023-12-11 20:48:11 -0500

    Code for Question 1# 10
    Code for Q 10.

---
+++ NET022Exam2Code/Question10/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Dictionary<string, string> d;
+
+        }
+
+
+        public class GenericClass<T> where T:struct
+        {
+
+            private List<T> collection;
+
+            public GenericClass()
+            {
+                collection = new List<T>();
+            }
+            public void AddItem(T item)
+            {
+                collection.Add(item);
+
+            }
+            public T GetItem(int index)
+            {
+                try
+                {
+                    return collection[index];
+                }
+                catch
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+
+            public IList<T> GetSorted()
+            {
+                var sortedC = new List<T>();
+                collection.AddRange ( sortedC);
+                sortedC.Sort();
+                return sortedC;
+            }
+        }
+
+
+
+    }
+}
